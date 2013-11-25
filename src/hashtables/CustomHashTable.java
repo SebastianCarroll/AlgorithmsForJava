cc3716e3315d5fedbfda3bb387334d0f164ab7df
Sebastian Carroll <sjc@localhost.localdomain>
2013-11-26 07:21:37 +1100

    Removed initial HashTable attempt. It was useless.

--- src/hashtables/CustomHashTable.java
+++ src/hashtables/CustomHashTable.java
@@ -4,27 +4,64 @@ package hashtables;
  * Basic implementation of a HashTable.
  * Currently supports:
  * 	- setting size of storage initially
- * 	- Putting and Getting of key, value String Pairs
+ * 	- Putting and Getting of key, value Strings Pairs
+ *  - Blocking Resizing of array when load factor reaches cutoff
+ * TODO:
+ *  - Deal with collisions - currently just overwrites if a key
+ *  	hashes to the same slot
+ *  - Dynamic resizing to reduce wait time
  * @author sjc
  */
 public class CustomHashTable {
-    String[] array;
+    
+    Node[] array;
+    double cutoffLoadFactor;
+    double currentLoadFactor = 0;
+    int count = 0;
+    
+    public CustomHashTable(int initialSize, double lfactor){
+        array = new Node[initialSize];
+        cutoffLoadFactor = lfactor;
+    }
     
     public CustomHashTable(int initialSize){
-        array = new String[initialSize];
+        array = new Node[initialSize];
+        cutoffLoadFactor = 0.75;
     }
     
     public String get(String key){
-        int k = hash(key);
-        return array[k];
+        int index = hash(key, array.length);
+        Node n = array[index];
+        return n.value;
     }
     
     public void put(String key, String val){
-        int k = hash(key);
-        array[k] = val;
+        if(count/array.length > cutoffLoadFactor) {
+            increaseArraySize();
+        }
+        int index = hash(key, array.length);
+        array[index] = new Node(key, val);
+        count++;
     }
     
-    private int hash(String key) {  
-        return (Math.abs(key.hashCode())) % array.length;  
+    private void increaseArraySize(){
+        int curLen = array.length;
+        Node[] newElems = new Node[curLen*2];
+        rehashElements(array, newElems);
+    }
+    
+    private void rehashElements(Node[] oldElements, Node[] newElements){
+        for(int i=0; i<oldElements.length; i++){
+            Node oldNode = oldElements[i];
+            if(oldNode != null){
+                int newIndex = hash(oldNode.key, newElements.length);
+                newElements[newIndex] = new Node(oldNode.key, oldNode.value);
+            }
+        }
+        array = newElements;
+    }
+    
+    private int hash(String key, int length) {  
+        return (Math.abs(key.hashCode())) % length;  
     }
 }
--- src/hashtables/CustomHashTableTest.java
+++ src/hashtables/CustomHashTableTest.java
@@ -12,13 +12,31 @@ public class CustomHashTableTest {
 	}
 
 	@Test
-	public void testPutAndGet() {
+	public void testGet() {
 		CustomHashTable cht = new CustomHashTable(10);
 		cht.put("apple", "apple pie with custard");
 		cht.put("orange", "apple pie with custard");
 		String val = cht.get("apple");
 		assertEquals("Check that getting and setting works", 
 				"apple pie with custard", 
+				val);
+	}
+	
+	@Test
+	public void testResize() {
+		CustomHashTable cht = new CustomHashTable(5);
+		cht.put("apple", "1");
+		cht.put("orange", "2");
+		cht.put("green", "3");
+		cht.put("red", "4");
+		cht.put("tuttle", "5");
+		cht.put("purple", "6");
+		cht.put("proof", "7");
+		cht.put("jim", "8");
+		cht.put("slow", "9");
+		String val = cht.get("tuttle");
+		assertEquals("Check that array correclty resizes", 
+				"5", 
 				val);
 	}
 
