76dbde79061471b3b65c1e528f9cd7ba20b9f1f0
Sebastian Carroll <sjc@localhost.localdomain>
2013-12-01 18:15:51 +1100

    Moved the Node class to a different package so it could be used by other classes aswell

---
+++ src/base/Node.java
@@ -0,0 +1,27 @@
+package base;
+
+public class Node {
+    public String key;
+    public String value;
+    public Node next;
+    
+    public Node(String k, String v){
+        key = k;
+        value = v;
+        next = null;
+    }
+    
+    public Node(String k, String v, Node n){
+        key = k;
+        value = v;
+        next = n;
+    }
+    
+    public Node deepCopy(){
+    	return new Node(key, value, next);
+    }
+    
+    public String toString(){
+    	return "(k: "+ key +",v: "+value+" )";
+    }
+}
