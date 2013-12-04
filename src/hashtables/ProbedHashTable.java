29db649ed290d0af411e6541b57766ad312eba14
Sebastian Carroll <sjc@localhost.localdomain>
2013-12-04 22:03:56 +1100

    Created and tested new HashTable class that resolves conflicts with linear probing

---
+++ src/hashtables/ProbedHashTable.java
@@ -0,0 +1,120 @@
+package hashtables;
+
+import base.*;
+
+/**
+ * Basic implementation of a HashTable which uses Linear Probing to resolve conflicts.
+ * Currently supports:
+ * 	- setting size of storage initially
+ * 	- Putting and Getting of key, value Strings Pairs
+ *  - Deal with collisions through Linear Probing
+ * TODO:
+ *  - Blocking Resizing of array when load factor reaches cutoff
+ *  - Dynamic resizing to reduce wait time
+ * @author sjc
+ */
+public class ProbedHashTable implements MapBase {
+	Node[] elements;
+	Double cutOffLoad = 0.75;
+	Double count = 0.0;
+	
+	public ProbedHashTable(){
+		elements = new Node[10];
+	}
+	
+	public ProbedHashTable(Integer initialSize){
+		elements = new Node[initialSize];
+	}
+	
+	public ProbedHashTable(Integer initialSize, Double initialCutOffLoad){
+		elements = new Node[initialSize];
+		cutOffLoad = initialCutOffLoad;
+	}
+
+	@Override
+	public void put(String key, String value) {
+		resizeIfNecessary();
+		int emptyIndex = findEmptyNodeIndex(key);
+		elements[emptyIndex] = new Node(key, value);
+		count++;
+	}
+
+	@Override
+	public String get(String key) {
+		Integer matched = findMatchingNodeIndex(key);
+		return matched != null ? elements[matched].value : null;
+	}
+
+	@Override
+	public Boolean containsKey(String key) {
+		Integer matched = findMatchingNodeIndex(key);
+		return matched != null;
+	}
+
+	@Override
+	public void delete(String key) {
+		Integer matched = findMatchingNodeIndex(key);
+		elements[matched].empty();
+	}
+	
+	private void resizeIfNecessary(){
+		if(count/elements.length > cutOffLoad ){
+			resizeTable();
+		}
+	}
+	
+	private void resizeTable(){
+		// Using a secondary HashMap to ease the resize code.
+		// Seems sub-optimal though I'm not sure why
+		ProbedHashTable moreElems = new ProbedHashTable(elements.length * 2);
+		for(Node old : elements){
+			if(old == null || old.key == null) continue;
+			moreElems.put(old.key, old.value);
+		}
+		elements = moreElems.elements.clone();
+	}
+	
+    private int hash(String key, int length) {  
+        return (Math.abs(key.hashCode())) % length;  
+    }
+    
+    private int findEmptyNodeIndex(String key){
+    	int index = getIndex(key);
+    	int slotsConsidered = 0;
+		Node position = elements[index];
+		while(position != null && slotsConsidered < elements.length){
+			index = getNextIndex(index);
+			position = elements[index];
+			slotsConsidered++;
+		} 
+		return index;
+    }
+    
+    private Integer findMatchingNodeIndex(String key){
+    	int index = getIndex(key);
+    	int startIndex = index;
+		Node position = elements[index];
+		
+		if(position == null){
+			return null;
+		}
+		
+		while(position.key != key){
+			index = getNextIndex(index);
+			position = elements[index];
+			if(index == startIndex || position == null){
+				return null;
+			}
+		} 
+		return index;
+    }
+    
+	private int getIndex(String key){
+		return hash(key, elements.length);
+	}
+    
+    private int getNextIndex(int index){
+    	return index == elements.length -1 ? 0 : ++index;
+    }
+	
+}
---
+++ src/hashtables/ProbedHashTableTest.java
@@ -0,0 +1,93 @@
+package hashtables;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProbedHashTableTest {
+
+	@Test
+	public void testGetAndPut() {
+		ProbedHashTable pht = new ProbedHashTable();
+		pht.put("key1", "value1");
+		pht.put("key2", "value2");
+		pht.put("key3", "value3");
+		pht.put("key4", "value4");
+		pht.put("key5", "value5");
+		String value = pht.get("key5");
+		assertEquals("Check last key inserted", 
+				"value5", 
+				value);
+		assertEquals("Check first key inserted", 
+				"value1", 
+				pht.get("key1"));
+	}
+	
+	@Test
+	public void testResizing() {
+		ProbedHashTable pht = new ProbedHashTable(5);
+		pht.put("key1", "value1");
+		pht.put("key2", "value2");
+		pht.put("key3", "value3");
+		pht.put("key4", "value4");
+		pht.put("key5", "value5");
+		pht.put("key11", "value11");
+		pht.put("key21", "value21");
+		pht.put("key31", "value31");
+		pht.put("key41", "value41");
+		pht.put("key51", "value51");
+		
+		assertEquals("Check last key inserted", 
+				"value51", 
+				pht.get("key51"));
+		assertEquals("Check first key inserted", 
+				"value1", 
+				pht.get("key1"));
+		assertEquals("Check table size", 
+				20, 
+				pht.elements.length);
+	}
+
+	@Test
+	public void testContainsKey() {
+		ProbedHashTable pht = new ProbedHashTable();
+		pht.put("key1", "value1");
+		pht.put("key2", "value2");
+		pht.put("key3", "value3");
+		pht.put("key4", "value4");
+		pht.put("key5", "value5");
+		assertEquals("Check last key inserted", 
+				true, 
+				pht.containsKey("key5"));
+		assertEquals("Check first key inserted", 
+				true, 
+				pht.containsKey("key1"));
+		assertEquals("Check key never inserted", 
+				false, 
+				pht.containsKey("keeet"));
+	}
+
+	@Test
+	public void testDelete() {
+		ProbedHashTable pht = new ProbedHashTable();
+		pht.put("key1", "value1");
+		pht.put("key2", "value2");
+		pht.put("key3", "value3");
+		pht.put("key4", "value4");
+		pht.put("key5", "value5");
+		pht.delete("key4");
+		assertEquals("Check last key inserted", 
+				false, 
+				pht.containsKey("key4"));
+		assertEquals("Check first key inserted", 
+				null, 
+				pht.get("key4"));
+		assertEquals("Check last key inserted", 
+				"value3", 
+				pht.get("key3"));
+		assertEquals("Check first key inserted", 
+				true, 
+				pht.containsKey("key3"));
+	}
+
+}
