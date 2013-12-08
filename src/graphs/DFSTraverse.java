e71bea30036edcda04adc2fd6efa75da432127e3
SebastianCarroll <seabass25@hotmail.com>
2013-12-08 14:05:16 +1100

    Created the DFSTraverse code and tests

---
+++ src/graphs/DFSTraverse.java
@@ -0,0 +1,34 @@
+package graphs;
+
+public class DFSTraverse extends DFS {
+	
+	String traversal = "";
+	TraversalOrder order;
+
+	public DFSTraverse(GraphNode[] ns, Integer[][] es, Integer sn, TraversalOrder o) {
+		super(ns, es, sn);
+		order = o;
+	}
+	
+	@Override
+	protected void DFSVisit(GraphNode node){
+		time++;
+		addToTraversal(TraversalOrder.Preorder, node);
+		markDiscovered(node);
+		exploreNeighbours(node);
+		markExplored(node);
+		addToTraversal(TraversalOrder.Postorder, node);
+	}
+	
+	private void addToTraversal(TraversalOrder expected, GraphNode node){
+		if(expected == order){
+			traversal += node.stringify();
+		}
+	}
+	
+	public String getTraversal(){
+		return traversal;
+	}
+	
+
+}
---
+++ src/graphs/DFSTraverseTest.java
@@ -0,0 +1,51 @@
+package graphs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DFSTraverseTest {
+	
+	@Test
+	public void testPreorder() {
+		Integer[][] graph = new Integer[][]{
+				{0,0,0,0},
+				{1,0,1,1},
+				{0,0,0,0},
+				{0,0,1,0}
+		};
+		Graph G = new Graph(graph);
+		
+        String traversal = G.traverse(1, TraversalOrder.Preorder);
+        assertEquals("(1)(0)(2)(3)", traversal);
+	}
+	
+	@Test
+	public void testPostorder() {
+		Integer[][] graph = new Integer[][]{
+				{0,0,0,0},
+				{1,0,1,1},
+				{0,0,0,0},
+				{0,0,1,0}
+		};
+		Graph G = new Graph(graph);
+		
+        String traversal = G.traverse(1, TraversalOrder.Postorder);
+        assertEquals("(0)(2)(3)(1)", traversal);
+	}
+	
+	@Test
+	public void testPostorderUndirected() {
+		Integer[][] graph = new Integer[][]{
+				{0,1,0,1},
+				{1,0,1,1},
+				{0,1,0,0},
+				{1,1,0,0}
+		};
+		Graph G = new Graph(graph);
+		
+        String traversal = G.traverse(1, TraversalOrder.Postorder);
+        assertEquals("(3)(0)(2)(1)", traversal);
+	}
+
+}
