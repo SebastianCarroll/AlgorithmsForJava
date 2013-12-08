2a032c4c3cfeb417c9db13d7091ea1c80c688210
SebastianCarroll <seabass25@hotmail.com>
2013-12-08 12:54:19 +1100

    Corrected copy errors. Targeted DFS not BFS

--- src/graphs/DFSTest.java
+++ src/graphs/DFSTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class DFSTest {
 	
 	@Test
-	public void checkBFS() {
+	public void checkDFS() {
 		Integer[][] graph = new Integer[][]{
 				{0,1,0,0},
 				{0,0,0,1},
@@ -17,7 +17,7 @@ public class DFSTest {
 		};
 		Graph G = new Graph(graph);
 		
-        GraphNode[] nodes = G.BFS(0);
+        GraphNode[] nodes = G.DFS(0);
         assertEquals(nodes[0].parent, null);
         assertEquals(nodes[1].parent.value, new Integer(0));
         assertEquals(nodes[2].parent.value, new Integer(3));
@@ -25,7 +25,7 @@ public class DFSTest {
 	}
 	
 	@Test
-	public void checkBFS_SelfPointingNode() {
+	public void checkDFS_SelfPointingNode() {
 		Integer[][] graph = new Integer[][]{
 				{0,1,0,0},
 				{0,1,0,1},
@@ -34,7 +34,7 @@ public class DFSTest {
 		};
 		Graph G = new Graph(graph);
 		
-        GraphNode[] nodes = G.BFS(0);
+        GraphNode[] nodes = G.DFS(0);
         assertEquals(nodes[0].parent, null);
         assertEquals(nodes[1].parent.value, new Integer(0));
         assertEquals(nodes[2].parent.value, new Integer(3));
@@ -42,7 +42,7 @@ public class DFSTest {
 	}
 	
 	@Test
-	public void checkBFS_UnreachableNode() {
+	public void checkDFS_UnreachableNode() {
 		Integer[][] graph = new Integer[][]{
 				{0,1,0,0},
 				{0,1,0,1},
@@ -51,28 +51,28 @@ public class DFSTest {
 		};
 		Graph G = new Graph(graph);
 		
-        GraphNode[] nodes = G.BFS(1);
+        GraphNode[] nodes = G.DFS(1);
         assertNull(nodes[0].parent);
-        assertEquals(nodes[0].colour, Colour.WHITE);
+        assertEquals(nodes[0].colour, Colour.BLACK);
         assertNull(nodes[1].parent);
         assertEquals(nodes[2].parent.value, new Integer(3));
         assertEquals(nodes[3].parent.value, new Integer(1));
 	}
 	
 	@Test
-	public void checkBFS_SingleNode() {
+	public void checkDFS_SingleNode() {
 		Integer[][] graph = new Integer[][]{
 				{1}
 		};
 		Graph G = new Graph(graph);
 		
-        GraphNode[] nodes = G.BFS(0);
+        GraphNode[] nodes = G.DFS(0);
         assertNull(nodes[0].parent);
         assertEquals(nodes[0].colour, Colour.BLACK);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void checkBFS_InvalidInput() {
+	public void checkDFS_InvalidInput() {
 		Integer[][] graph = new Integer[][]{
 				{1, 0, 0}
 		};
@@ -80,12 +80,12 @@ public class DFSTest {
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void checkBFS_StartNodeOutOfBounds() {
+	public void checkDFS_StartNodeOutOfBounds() {
 		Integer[][] graph = new Integer[][]{
 				{1}
 		};
 		Graph G = new Graph(graph);
-		G.BFS(1);
+		G.DFS(1);
 	}
 
 }
