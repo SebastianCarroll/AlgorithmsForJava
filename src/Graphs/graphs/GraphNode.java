389b7c377c8d387beab01a2def53f9351413093f
SebastianCarroll <seabass25@hotmail.com>
2013-12-09 21:04:42 +1100

    Modified graphnode to include a key

--- src/Graphs/graphs/GraphNode.java
+++ src/Graphs/graphs/GraphNode.java
@@ -2,11 +2,12 @@ package Graphs.graphs;
 
 import Misc.base.Colour;
 
-public class GraphNode {
+public class GraphNode implements Comparable<GraphNode> {
 	Colour colour;
 	Integer d;
 	Integer f;
 	GraphNode parent;
+	Integer key;
 	Integer value;
 	
 	public GraphNode(Integer v){
@@ -23,5 +24,10 @@ public class GraphNode {
 	
 	public String stringify(){
 		return "(" + value + ")";
+	}
+
+	@Override
+	public int compareTo(GraphNode node) {
+		return value.compareTo(node.value);
 	}
 }
