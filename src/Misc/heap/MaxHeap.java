81f4ccb8724384316264bf1902bca600bb7c5901
SebastianCarroll <seabass25@hotmail.com>
2013-12-09 21:04:57 +1100

    Used the get/set methods instead of the old way

--- src/Misc/heap/MaxHeap.java
+++ src/Misc/heap/MaxHeap.java
@@ -15,8 +15,8 @@ public class MaxHeap<E extends Comparable<E>> extends Heap<E> {
 	// Use -1 to account for mandatory 1th indexed heap
 	protected Boolean inOrder(Integer l, Integer r){
 		// True if left < right
-		E left = heap.get(l-1);
-		E right = heap.get(r-1);
+		E left = getAt(l);
+		E right = getAt(r);
 		return left.compareTo(right) >= 0;
 	}
 }
