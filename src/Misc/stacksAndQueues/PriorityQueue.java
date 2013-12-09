d0678302c0b9987d622cf170e06cf6bf5fe9d1b5
SebastianCarroll <seabass25@hotmail.com>
2013-12-09 21:04:31 +1100

    Partial implementation of a priority queue

--- src/Misc/stacksAndQueues/PriorityQueue.java
+++ src/Misc/stacksAndQueues/PriorityQueue.java
@@ -1,5 +1,16 @@
 package Misc.stacksAndQueues;
 
+import Misc.heap.*;
+
 public class PriorityQueue<E extends Comparable<E>> {
+	MinHeap<E> heap;
 	
+	public PriorityQueue(E[] initialElements){
+		heap = new MinHeap<E>(initialElements);
+	}
+	
+	public void insert(E newElement){
+		
+		
+	}
 }
