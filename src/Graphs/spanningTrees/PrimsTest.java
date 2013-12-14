a1df8b1fd43f1cef365d112aa7e61e6588d00048
SebastianCarroll <seabass25@hotmail.com>
2013-12-14 11:23:44 +1100

    Added tests for Prims. They all fail atm

---
+++ src/Graphs/spanningTrees/PrimsTest.java
@@ -0,0 +1,44 @@
+package Graphs.spanningTrees;
+
+import static org.junit.Assert.*;
+
+import Graphs.graphs.*;
+
+import org.junit.Test;
+
+import Graphs.graphs.Graph;
+
+public class PrimsTest {
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrims() {
+		Integer[][] edges = new Integer[][]{
+				{0,1,2,3},
+				{0,0,0,1},
+				{0,0,0,0},
+				{0,0,0,0}
+		};
+		
+		GraphNode[] nodes = new GraphNode[]{
+				new GraphNode(1, 2),
+				new GraphNode(2, 2),
+				new GraphNode(3, 3),
+				new GraphNode(4, 4),
+		};
+		
+		Graph G = new Graph(edges, nodes);
+		
+		GraphNode[] mst = G.Prims(0);
+		
+		assertNull(mst[0].parent);
+		assertEquals(mst[1].parent.key, new Integer(0));
+		assertEquals(mst[2].parent.key, new Integer(0));
+		assertEquals(mst[3].parent.key, new Integer(1));
+	}
+
+}
